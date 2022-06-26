dfa802e2d465b5e1fdd69887e797f7979641d8e2
benpbo <prizeben@gmail.com>
2022-06-26 09:48:58 +0300

    Add temperature route

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,6 +1,9 @@
+import mimetypes
 import os
 
-from flask import Flask
+from flask import Flask, Response, request
+
+from .data import Element, Scenario, get_model_data
 
 
 def create_app(test_config=None):
@@ -19,5 +22,24 @@ def create_app(test_config=None):
         os.makedirs(app.instance_path)
     except OSError:
         pass
+
+    @app.route('/temperature', methods=['GET'])
+    def temperture():
+        station = request.args.get('station', type=str)
+        model = request.args.get('model', type=str)
+        match request.args.get('scenario', type=str):
+            case 'rcp45':
+                scenario = Scenario.RCP45
+            case 'rcp85':
+                scenario = Scenario.RCP85
+            case None:
+                return 'No scenario', 400
+            case unmatched:
+                return f'Bad scenario: {unmatched}', 400
+
+        data = get_model_data(
+            Element.TEMP_AVG, (station, ), (model, ), scenario)
+
+        return Response(data.to_csv(None), mimetype=mimetypes.types_map['.csv'])
 
     return app
