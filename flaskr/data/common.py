c1d2fd6e3d5f6e1aa9c227b9dd1ebd5bc8430f33
benpbo <prizeben@gmail.com>
2022-07-13 23:50:43 +0300

    Move _add_value_column to data.common module
    Rename _add_value_column to tranform_to_graph.
    Move constants to data.common module.
    Add column discaring to transform_to_graph.

--- flaskr/data/common.py
+++ flaskr/data/common.py
@@ -1,5 +1,16 @@
 from enum import Enum, auto
 
+from pandas import DataFrame
+
+STATION_COL = 'station'
+YEAR_COL = 'year'
+MONTH_COL = 'month'
+DAY_COL = 'day'
+TEMP_MIN_COL = 'tmin'
+TEMP_MAX_COL = 'tmax'
+RAIN_MM_COL = 'pr'
+VALUE_COL = 'value'
+GRAPH_COLUMNS = [YEAR_COL, MONTH_COL, DAY_COL, VALUE_COL]
 
 class Element(Enum):
     TEMP_MAX = auto()
@@ -16,8 +27,16 @@ class Element(Enum):
                 return 'rain'
 
 
-YEAR_COL = 'year'
-MONTH_COL = 'month'
-DAY_COL = 'day'
-VALUE_COL = 'value'
-REQUIRED_COLUMNS = [YEAR_COL, MONTH_COL, DAY_COL, VALUE_COL]
+def transform_to_graph(df: DataFrame, element: Element) -> DataFrame:
+    match element:
+        case Element.TEMP_MIN:
+            graph = df.rename({TEMP_MIN_COL: VALUE_COL})
+        case Element.TEMP_MAX:
+            graph = df.rename({TEMP_MAX_COL: VALUE_COL})
+        case Element.TEMP_AVG:
+            temp_avg = df[[TEMP_MIN_COL, TEMP_MAX_COL]].mean(axis=1)
+            graph = df.assign(**{VALUE_COL: temp_avg})
+        case Element.RAIN_MM:
+            graph = df.rename({RAIN_MM_COL: VALUE_COL})
+
+    return graph[GRAPH_COLUMNS]
--- flaskr/data/model_data.py
+++ flaskr/data/model_data.py
@@ -5,14 +5,10 @@ from typing import IO, Callable, Iterable
 import pandas as pd
 from pandas import DataFrame
 
-from .common import REQUIRED_COLUMNS, VALUE_COL, Element
+from .common import STATION_COL, Element, transform_to_graph
 
-TEMP_MIN_COL = 'tmin'
-TEMP_MAX_COL = 'tmax'
-RAIN_MM_COL = 'pr'
 SCENARIO_COL = 'scenario'
 MODEL_COL = 'model'
-STATION_COL = 'station'
 
 
 class Scenario(Enum):
@@ -37,21 +33,4 @@ def get_model_data(
     model_mask = df[MODEL_COL].isin(model_names)
     df = df[station_mask & scenario_mask & model_mask]
 
-    # Add value column
-    df = _add_value_column(df, element)
-
-    # Return the dataframe with the required columns
-    return df[REQUIRED_COLUMNS]
-
-
-def _add_value_column(df: DataFrame, element: Element) -> DataFrame:
-    match element:
-        case Element.TEMP_MIN:
-            return df.rename({TEMP_MIN_COL: VALUE_COL})
-        case Element.TEMP_MAX:
-            return df.rename({TEMP_MAX_COL: VALUE_COL})
-        case Element.TEMP_AVG:
-            df[VALUE_COL] = df[[TEMP_MIN_COL, TEMP_MAX_COL]].mean(axis=1)
-            return df
-        case Element.RAIN_MM:
-            return df.rename({RAIN_MM_COL: VALUE_COL})
+    return transform_to_graph(df, element)
