1dd6c41b0ad8468c85bbec7a2905ced13e092962
benpbo <prizeben@gmail.com>
2022-07-15 17:31:26 +0300

    Implement get_observation_data function

--- flaskr/data/observation_data.py
+++ flaskr/data/observation_data.py
@@ -1,6 +1,10 @@
+from os import path
 from typing import IO, Callable, Iterable
 
+import pandas as pd
 from pandas import DataFrame
+
+from flaskr.data.filters import StationFilter
 
 from .common import Element
 
@@ -9,4 +13,11 @@ def get_observation_data(
         reader: Callable[[str], IO],
         element: Element,
         station_names: Iterable[str]) -> DataFrame:
-    raise NotImplementedError()
+    # Read csv
+    file_name = path.join('db', 'observations', f'{element.type}.csv')
+    with reader(file_name) as file:
+        df = pd.read_csv(file)
+
+    # Apply filters
+    df_filter = StationFilter(station_names)
+    return df_filter.filter(df)
