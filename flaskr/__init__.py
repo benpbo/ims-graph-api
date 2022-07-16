b97d32eede4e2d08b67da3689dc64942ea9ef440
benpbo <prizeben@gmail.com>
2022-07-16 13:14:17 +0300

    Break down get_graph route
    Remove get_graph function.
    Add get_observations_graph function under /graph/observations route.
    Add get_models_graph function under /graph/models/route.

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,13 +1,31 @@
 import http
 import os
+from typing import Any
 
-from flask import Flask, jsonify, request
+from flask import Flask, Response, jsonify
+from marshmallow_enum import EnumField
+from pandas import DataFrame
 from webargs import fields
 from webargs.flaskparser import use_args
 
 from flaskr.data.common import transform_to_graph
 
 from .data import Element, Scenario, get_model_data, get_observation_data
+
+OBSERVATIONS_GRAPH_ARGS = {
+    'station': fields.List(fields.Str, required=True),
+}
+
+MODELS_GRAPH_ARGS = {
+    **OBSERVATIONS_GRAPH_ARGS,
+    'scenario': EnumField(Scenario, required=True),
+    'model': fields.List(fields.Str, required=True),
+}
+
+
+def create_response(data: DataFrame, element: Element) -> Response:
+    graph = transform_to_graph(data, element)
+    return jsonify(graph.to_dict(orient='records'))
 
 
 def create_app(test_config=None):
@@ -27,31 +45,34 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    @app.route('/graph', methods=['GET'])
+    @app.route('/graph/observations', methods=['GET'])
     @use_args(
-        {
-            'station': fields.List(fields.Str, required=True),
-        },
+        OBSERVATIONS_GRAPH_ARGS,
         error_status_code=http.HTTPStatus.BAD_REQUEST,
         location='query')
-    def get_graph(args):
+    def get_observations_graph(args: dict[str, Any]):
         element = Element.TEMP_AVG
-        stations: list[str] = args['station']
+        data = get_observation_data(
+            app.open_resource,
+            element,
+            args['station'])
 
-        if 'scenario' in request.args:
-            scenario = request.args.get('scenario', type=Scenario)
-            models = request.args.getlist('model', type=str)
-            if scenario is None or not models:
-                return 'At least one of the following query parameters is missing: scenario, models.', 400
+        return create_response(data, element)
 
-            data = get_model_data(
-                app.open_resource, element, stations, models, scenario)
-        else:
-            data = get_observation_data(
-                app.open_resource, element, stations)
+    @app.route('/graph/models', methods=['GET'])
+    @use_args(
+        MODELS_GRAPH_ARGS,
+        error_status_code=http.HTTPStatus.BAD_REQUEST,
+        location='query')
+    def get_models_graph(args: dict[str, Any]):
+        element = Element.TEMP_AVG
+        data = get_model_data(
+            app.open_resource,
+            element,
+            args['station'],
+            args['model'],
+            args['scenario'])
 
-        graph = transform_to_graph(data, element)
-
-        return jsonify(graph.to_dict(orient='records'))
+        return create_response(data, element)
 
     return app
