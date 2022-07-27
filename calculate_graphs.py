f9334f4d6768b13cd6a47d2b5dfdfb69777c32dc
benpbo <prizeben@gmail.com>
2022-07-27 11:34:20 +0300

    Add call to flask API in calculation script

--- calculate_graphs.py
+++ calculate_graphs.py
@@ -15,6 +15,7 @@
 # df_predictions = year, month, day, value, station, model, scenario
 # df_line = Year, month, day, value, station
 
+import requests
 import pandas as pd
 
 ### CONSTS ###
@@ -45,18 +46,26 @@ MIN_FUNC = min
 MAX_FUNC = max
 
 # Get from user
+element = 'TEMP_AVG'
+models_list = ['cnrm_cclm']
+scenarios = ('RCP45', 'RCP85')
+stations_list = ['afula']
+resolution = [1, 2]
 data_time_range = ('1950', '2020')
-line_time_range = ('1950', '2020')
+line_time_range = ('1961', '1990')
 show_all_models = True
 show_average = True
 show_line = True
 calculation = MIN_FUNC
-data = {'observations': True, 'models': ['cnrm_cclm', 'had_cclm', 'knmi_racm', 'mpi_remo', 'cccma_rca', 'cnrm_rca', 'csiro_rca', 'ipsl_rca', 'miroc_rca', 'had_rca', 'mpi_rca', 'noaa_rca'], 'scenario': ['rcp85']}
+data = {'observations': True, 'models': ['cnrm_cclm', 'had_cclm', 'knmi_racm', 'mpi_remo', 'cccma_rca',
+                                         'cnrm_rca', 'csiro_rca', 'ipsl_rca', 'miroc_rca', 'had_rca', 'mpi_rca', 'noaa_rca'], 'scenario': ['rcp85']}
 
 ### FUNCTIONS ###
 
+
 def get_relevant_stations(dfs) -> list:
-    min_year = (10000, [])   # tuple of minimum year and the df that has the minimum year
+    # tuple of minimum year and the df that has the minimum year
+    min_year: tuple[int, list] = (10000, [])
     for df in dfs:
         if not df.empty:
             min_year = (min(min_year[0], df[YEAR_COL].min()), df)
@@ -64,25 +73,63 @@ def get_relevant_stations(dfs) -> list:
     df = df[df[YEAR_COL] == min_year[0]]
     return list(df[STATION_COL].drop_duplicates())
 
+
 def calculate_df(df, calculation_func) -> pd.DataFrame:
     if calculation_func in [MIN_FUNC, MAX_FUNC]:
-        df = df.groupby([YEAR_COL, STATION_COL])[DATA_COL].apply(calculation_func)
+        df = df.groupby([YEAR_COL, STATION_COL])[
+            DATA_COL].apply(calculation_func)
         df = df.reset_index(level=0)
     return df.groupby(YEAR_COL)[DATA_COL].apply(pd.DataFrame.mean)
+
 
 def df_add_col(original_df, new_col_name, data, calculation):
     return pd.concat([original_df, pd.DataFrame({new_col_name: calculate_df(data, calculation)})], axis=1, levels=0)
 
+
 # QUERY DB
-#********************** FILL
-df_observations = pd.read_csv(TEST_CSV_MIN).dropna()
-df_predictions = pd.read_csv(TEST_CSV_MODEL_DOROT_MIN).dropna()
-df_line = pd.read_csv(TEST_CSV_MIN).dropna()
+# ********************** FILL
+url = 'http://localhost:5000'
+base_params = {
+    'element': element,
+    'station': stations_list,
+    'start_year': data_time_range[0],
+    'end_year': data_time_range[1],
+    'month': resolution,
+}
+
+df_observations = pd.read_json(requests.get(
+    f'{url}/graph/observations',
+    params=base_params).text)
+df_predictions = pd.concat(
+    pd.read_json(requests.get(
+        f'{url}/graph/predictions',
+        params={
+            **base_params,
+            'model': models_list,
+            'scenario': scenario,
+        }).text)
+    for scenario in scenarios)
+df_line = pd.read_json(requests.get(
+    f'{url}/graph/observations',
+    params={
+        **base_params,
+        'start_year': line_time_range[0],
+        'end_year': line_time_range[1],
+    }).text)
+
+print(df_observations)
+print(df_predictions)
+print(df_line)
 
 # calculate minimum year and return only relevant stations
 
-stations_list = get_relevant_stations([df_observations, df_predictions, df_line])
-fix_data = lambda df: df[df[STATION_COL].isin(stations_list)]
+stations_list = get_relevant_stations(
+    [df_observations, df_predictions, df_line])
+
+
+def fix_data(df): return df[df[STATION_COL].isin(stations_list)]
+
+
 df_observations_new = fix_data(df_observations)
 df_predictions_new = fix_data(df_predictions)
 
@@ -91,16 +138,19 @@ df_predictions_new = fix_data(df_predictions)
 df_graphs = pd.DataFrame()
 
 if not df_observations_new.empty:
-    df_graphs = df_add_col(df_graphs, OBSERVATIONS_GRAPH_NAME, df_observations_new, calculation)
+    df_graphs = df_add_col(df_graphs, OBSERVATIONS_GRAPH_NAME,
+                           df_observations_new, calculation)
 if not df_predictions_new.empty:
     if show_all_models:
         model_list = df_predictions_new[MODEL_COL].drop_duplicates()
         for model in model_list:
-            df_graphs = df_add_col(df_graphs, model, df_predictions_new[df_predictions_new[MODEL_COL] == model], calculation)
+            df_graphs = df_add_col(
+                df_graphs, model, df_predictions_new[df_predictions_new[MODEL_COL] == model], calculation)
     if show_average:
         scenario_list = df_predictions_new[SCENARIO_COL].drop_duplicates()
         for scenario in scenario_list:
-            df_graphs = df_add_col(df_graphs, scenario + '_avg', df_predictions_new[df_predictions_new[SCENARIO_COL] == scenario], calculation)
+            df_graphs = df_add_col(
+                df_graphs, scenario + '_avg', df_predictions_new[df_predictions_new[SCENARIO_COL] == scenario], calculation)
 if not df_line.empty:
     if show_line:
         df_graphs[BASE_LINE] = calculate_df(df_line, calculation).mean()
